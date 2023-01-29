d61dca5f22bb355ddef85b36056a371b69120ab0
Seiya Miyazaki <smsmiyazaki@gmail.com>
2023-01-29 12:37:05 -0500

    setting up _req placeholders

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const {notes} = require('../../db/db.json');
 const {createNote} = require ('../../lib/noteFunctions');
 
-router.get('/notes', (req, res) => {
+router.get('/notes', (_req, res) => {
     res.json(notes);
 })
 
