570ce6f332f952f879f465d47b6952b18a75bdd0
Seiya Miyazaki <smsmiyazaki@gmail.com>
2023-01-29 14:23:30 -0500

    adjusting static address

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true }));
 app.use('/api', api);
 app.use('/', html);
-app.use(express.static('public'));
+
 
 
 app.listen(PORT, () => {
