bfe78d47fb4f82dcdbca66ff15a8b2a3f465446f
Seiya Miyazaki <smsmiyazaki@gmail.com>
2023-01-30 22:23:24 -0500

    Fixing adding and deleting notes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,6 +1,9 @@
 const router = require("express").Router(); // Import the express.Router function
 const { notes } = require("../../db/db.json"); // Import notes array from JSON
-const createNote = require("../../lib/noteFunctions"); // Import create note function
+const {
+    createNote,
+    deleteNote
+} = require('../../lib/noteFunctions'); // Import create note function
 
 router.get("/notes", (_req, res) => { // Route to get all routes, added _to req to show it's deprecated
   res.json(notes);
@@ -11,5 +14,8 @@ router.post("/notes", (req, res) => { // Route to create new note
   let note = createNote(req.body, notes); // Creating a new note using the createNote function from earlier
   res.json(note);
 });
-
+router.delete('/notes/:id', (req, res) => {
+    deleteNote(notes, req.params.id);
+    res.json(notes);
+})
 module.exports = router; // Exporting the module again
